e4e55c6f72ddf0c380ff741c696fe12a8f79edbe
baotoan-dev <20161385@student.hcmute.edu.vn>
2024-06-08 21:08:55 +0700

    remove firebase

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import * as SecureStore from 'expo-secure-store';
 import ChatContextProvider from './contex/ChatContex';
-// import messaging from '@react-native-firebase/messaging';
 
 export const AuthContext = createContext();
 
@@ -28,48 +27,6 @@ export default function App() {
     checkAuth();
   }, []);
 
-  // async function requestUserPermission() {
-  //   const authStatus = await messaging().requestPermission();
-  //   const enabled =
-  //     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-  //     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-  //   if (enabled) {
-  //     console.log('Authorization status:', authStatus);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   if (requestUserPermission) {
-  //     messaging().getToken().then(token => {
-  //       console.log(token);
-  //     });
-  //   }
-  //   else {
-  //     console.log('no token');
-  //   }
-
-  //   messaging()
-  //     .getInitialNotification()
-  //     .then(async (remoteMessage) => {
-  //       if (remoteMessage) {
-  //         console.log(
-  //           'Notification caused app to open from quit state:',
-  //           remoteMessage.notification,
-  //         );
-  //       }
-  //     });
-
-  //   messaging().setBackgroundMessageHandler(async remoteMessage => {
-  //     console.log('Message handled in the background!', remoteMessage);
-  //   })
-
-  //   const unsubscribe = messaging().onMessage(async remoteMessage => {
-  //     console.log('A new FCM message arrived!', remoteMessage);
-  //   });
-
-  //   return unsubscribe;
-  // }, []);
   return (
     <Provider store={store}>
       <ChatContextProvider>
